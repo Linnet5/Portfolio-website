1f45a1e39fafe360fe56d3d8267b3dd70a8502cb
Linnet5 <itslinnet@gmail.com>
2022-06-01 17:59:46 +0200

    Fixed shit

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:linnet5/linnet5.github.io.git main
+#git push -f git@github.com:linnet5/linnet5.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Linnet5/Portfolio-website.git main:gh-pages
 
 cd -
