58e45749281aa892e5131a0be6ab6ab2d479babc
Linnet5 <itslinnet@gmail.com>
2022-11-29 08:10:07 +0100

    added holmgang

--- src/App.jsx
+++ src/App.jsx
@@ -28,75 +28,77 @@ import { useState } from 'react'
 import cv from './cv.pdf';
 import './App.css';
 import projectsData from './projectsData.json';
+import currentProjectsData from './currentProjectsData.json';
 
 function App() {
   return (
     <>
-    <Flex className = "bg">
-      <Container maxW="container.xl" p={0} color = "whitesmoke">
-        <Introduction/>
-      </Container>
-    </Flex>
-    <Flex bg="whitesmoke">
-      <Container maxW="container.xl" p={0} color = "gray">
-        <Projects/>
-      </Container>
-    </Flex>
-    <Box paddingTop ={10} bg="whitesmoke">
-      <Flex bg="#1d2024">
-        <Container maxW="container.xl" color = "whitesmoke">
-          <Box paddingLeft={[0, 10]} paddingRight={[0, 10]}>
-            <Center>
-              <Link href="https://www.linkedin.com/in/linus-karlsson-503b881b7/" m ={10}>LinkedIn</Link>
-              <Link href="https://github.com/Linnet5" m ={10}>GitHub</Link>
-              <Link href= "mailto:linus.karlsson.96@hotmail.com" m ={10}>Contact</Link>
-            </Center>
-          </Box>
+      <Flex className="bg">
+        <Container maxW="container.xl" p={0} color="whitesmoke">
+          <Introduction />
         </Container>
       </Flex>
-    </Box>
+      <Flex bg="whitesmoke">
+        <Container maxW="container.xl" p={0} color="gray">
+          {/* <Projects data={currentProjectsData} header="Current Project" /> */}
+          <Projects data={projectsData} header="Projects" />
+        </Container>
+      </Flex>
+      <Box paddingTop={10} bg="whitesmoke">
+        <Flex bg="#1d2024">
+          <Container maxW="container.xl" color="whitesmoke">
+            <Box paddingLeft={[0, 10]} paddingRight={[0, 10]}>
+              <Center>
+                <Link href="https://www.linkedin.com/in/linus-karlsson-503b881b7/" m={10}>LinkedIn</Link>
+                <Link href="https://github.com/Linnet5" m={10}>GitHub</Link>
+                <Link href="mailto:linus.karlsson.96@hotmail.com" m={10}>Contact</Link>
+              </Center>
+            </Box>
+          </Container>
+        </Flex>
+      </Box>
     </>
   )
 }
 
 function Introduction() {
-  return(
+  return (
     <Box paddingLeft={[0, 10]} paddingRight={[0, 10]} paddingBottom={10}>
       <Flex>
-        <VStack id = "desc"
-          w = "75%" 
-          h="full" 
-          p={10} 
-          spacing = {5} 
+        <VStack id="desc"
+          w="75%"
+          h="full"
+          p={10}
+          spacing={5}
           alignItems="flex-start"
         >
-          <Text fontSize = '5xl' fontWeight="600" className = "reveal-text">Hello There!</Text>
-          <Text fontSize = 'xl'>I am Linus Karlsson, a software developer.</Text> 
+          <Text fontSize='5xl' fontWeight="600" className="reveal-text">Hello There!</Text>
+          <Text fontSize='xl'>I am Linus Karlsson, a software developer.</Text>
           <Text>Welcome to my page! I love to work with creative solutions and to fully immerse myself in an exciting project. I am an ambitious and independent worker, but work at my absolut best ability in a motivated team. I know how to communicate and I have a positive attitude. Right now I am doing my master studies in engineering for media technology at Linköping University.</Text>
         </VStack>
-        <VStack id = "profilePicture"
-          w="25%" 
-          h="full" 
-          p={10} 
+        <VStack id="profilePicture"
+          w="25%"
+          h="full"
+          p={10}
           alignItems="flex-start"
         >
-          <Box paddingTop={0}/>
-          <Image borderRadius = 'full' src="/images/portrait.jpg" alt='Dan Abramov' />
+          <Box paddingTop={0} />
+          <Image borderRadius='full' src="/images/portrait.jpg" alt='Dan Abramov' />
         </VStack>
       </Flex>
       <Center>
-        <CVModalSwe/>
-        <CVModalEng/>
+        <CVModalSwe />
+        <CVModalEng />
       </Center>
       <Center>
-        <Link 
+        <Link
           href="\src\cv.pdf"
           color="gray.400"
           fontSize="xs"
           paddingTop={1}
-          px = {2}
+          px={2}
         > (Direct Link)</Link>
-        <Link 
+        <Link
           href="\src\cv.pdf"
           color="gray.400"
           fontSize="xs"
@@ -107,16 +109,17 @@ function Introduction() {
   );
 }
 
-function Projects() {
-  return(
-    <Box paddingLeft={[0,10]} paddingRight={[0,10]}>
+function Projects(props) {
+  var data = props.data;
+  return (
+    <Box paddingLeft={[0, 10]} paddingRight={[0, 10]}>
       <Center>
-      <Flex p={[2, 10]}>
-        <Text fontSize = '4xl'>Projects</Text>
-      </Flex>
+        <Flex p={[2, 10]}>
+          <Text fontSize='4xl'>{props.header}</Text>
+        </Flex>
       </Center>
-      <Wrap paddingLeft={[0, 10]} paddingRight={[0, 10]} paddingBottom = {10} justify= "center"> 
-        {projectsData.projects.map(PostComponent)}
+      <Wrap paddingLeft={[0, 10]} paddingRight={[0, 10]} paddingBottom={10} justify="center">
+        {data.projects.map(PostComponent)}
       </Wrap>
     </Box>
   );
@@ -125,65 +128,66 @@ function Projects() {
 function PostComponent(props) {
   const { isOpen, onToggle } = useDisclosure()
 
-  return(
-  <WrapItem onLoad={onToggle}>
-    <ScaleFade in={isOpen} offsetY='20px'>
-          <PortfolioPost 
-            imageSrc = {props.Image}
-            name = {props.Name}
-            description = {props.Description}
-            type={props.Type}
-            link={props.Link}/>                
-    </ScaleFade>
-  </WrapItem>);
+  return (
+    <WrapItem onLoad={onToggle}>
+      <ScaleFade in={isOpen} offsetY='20px'>
+        <PortfolioPost
+          header={props.header}
+          imageSrc={props.Image}
+          name={props.Name}
+          description={props.Description}
+          type={props.Type}
+          link={props.Link} />
+      </ScaleFade>
+    </WrapItem>);
 }
 
 function CVModalSwe() {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  return(
+  return (
     <>
-    <Button onClick={onOpen} className = "button" bg="transparent" _hover={{ bg: '#2381e6' }} mx = {2}>View CV (Swe)</Button>
-    <Modal isOpen={isOpen} onClose={onClose} size = '3xl'>
-      <ModalOverlay />
-      <ModalContent h = "288mm" w ="100%">
-        <ModalBody >
-          <iframe src={`${cv}#view=fitW`} title="cv" height="100%" width="100%" />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+      <Button onClick={onOpen} className="button" bg="transparent" _hover={{ bg: '#2381e6' }} mx={2}>View CV (Swe)</Button>
+      <Modal isOpen={isOpen} onClose={onClose} size='3xl'>
+        <ModalOverlay />
+        <ModalContent h="288mm" w="100%">
+          <ModalBody >
+            <iframe src={`${cv}#view=fitW`} title="cv" height="100%" width="100%" />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
 
 function CVModalEng() {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  return(
+  return (
     <>
-    <Button onClick={onOpen} className = "button" bg="transparent" _hover={{ bg: '#2381e6' }}>View CV (Eng)</Button>
-    <Modal isOpen={isOpen} onClose={onClose} size = '3xl'>
-      <ModalOverlay />
-      <ModalContent h = "288mm" w ="100%">
-        <ModalBody >
-          <iframe src={`${cv}#view=fitW`} title="cv" height="100%" width="100%" />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+      <Button onClick={onOpen} className="button" bg="transparent" _hover={{ bg: '#2381e6' }}>View CV (Eng)</Button>
+      <Modal isOpen={isOpen} onClose={onClose} size='3xl'>
+        <ModalOverlay />
+        <ModalContent h="288mm" w="100%">
+          <ModalBody >
+            <iframe src={`${cv}#view=fitW`} title="cv" height="100%" width="100%" />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
 
 
 function PortfolioPost(props) {
-  const onCardClick = function() {
+  const onCardClick = function () {
     window.open(props.link);
   }
   return (
     <Center p={1}>
-      <Box 
-        id = "card"
-        onClick = {onCardClick}
-        _hover = {{transition: "0.2s", cursor: "pointer"}}
-        h = {'575px'}
+      <Box
+        id="card"
+        onClick={onCardClick}
+        _hover={{ transition: "0.2s", cursor: "pointer" }}
+        h={'575px'}
         maxW={'360px'}
         w={'full'}
         bg={useColorModeValue('white', 'gray.900')}
@@ -198,26 +202,26 @@ function PortfolioPost(props) {
           mx={-6}
           mb={6}
           pos={'relative'}>
-          <Image className = "projectImage"
+          <Image className="projectImage"
             src={props.imageSrc}
             fit="cover"
           />
         </Box>
         <Stack>
-          <Heading className = "projectName"
+          <Heading className="projectName"
             color={useColorModeValue('gray.700', 'white')}
             fontSize={'2xl'}
             fontFamily={'body'}
-            paddingTop = {10}
-            _groupHover = {{textDecoration: "underline"}}>
+            paddingTop={10}
+            _groupHover={{ textDecoration: "underline" }}>
             {props.name}
           </Heading>
-            <Text color={'gray.500'}>
-              {props.description}
-            </Text>
-            <Text color={'gray.700'} fontWeight="bold">
-              {props.type}
-            </Text>
+          <Text color={'gray.500'}>
+            {props.description}
+          </Text>
+          <Text color={'gray.700'} fontWeight="bold">
+            {props.type}
+          </Text>
         </Stack>
       </Box>
     </Center>
